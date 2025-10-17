69b82336ead356959f12740f949dba88c42b1f47
jamesadams <jamesadamspub@gmail.com>
2025-10-17 22:26:29 +0200

    test

--- wallos/plugin.php
+++ wallos/plugin.php
@@ -1,13 +1,23 @@
 <?php
-/*
-Plugin Name: WallOS Homepage Element
-Plugin Version: 1.0
-Plugin Author: James Adams
-Plugin Description: Ajoute un élément personnalisé à la homepage Organizr.
-Plugin Homepage: https://github.com/jamesadams/wallos-homepage
-*/
 
-class WallOS_Homepage_Plugin {
+$GLOBALS['plugins']['Wallos'] = array( // Plugin Name
+	'name' => 'Wallos', // Plugin Name
+	'author' => 'JamesAdams', // Who wrote the plugin
+	'category' => 'Utilities', // One to Two Word Description
+	'link' => '', // Link to plugin info
+	'license' => 'personal', // License Type use , for multiple
+	'idPrefix' => 'WALLOS', // html element id prefix (All Uppercase)
+	'configPrefix' => 'WALLOS', // config file prefix for array items without the hypen (All Uppercase)
+	'version' => '1.0.0', // SemVer of plugin
+	'image' => 'data/plugins/stripe/logo.png', // 1:1 non transparent image for plugin
+	'settings' => true, // does plugin need a settings modal?
+	'bind' => true, // use default bind to make settings page - true or false
+	'api' => 'api/v2/plugins/stripe/settings', // api route for settings page (All Lowercase)
+	'homepage' => true // Is plugin for use on homepage? true or false
+);
+
+class WallosPlugin extends Organizr  {
+
     public function __construct() {
         // Hook pour ajouter à la homepage
         add_hook('homepage', [$this, 'addHomepageElement']);
@@ -21,6 +31,3 @@ class WallOS_Homepage_Plugin {
               </div>';
     }
 }
-
-// Initialisation du plugin
-new WallOS_Homepage_Plugin();
